2c1045d3eb849dfd2eaa06e1e212d230fc708630
koom <hunmonhun@gmail.com>
2022-09-13 14:30:36 +0900

    블로그 메뉴 헤더에 추가

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -8,6 +8,7 @@ function Header() {
           <div className="header-nav-item inline-block text-left"><Link to="/" >홈</Link></div>
           <div className="header-nav-item inline-block text-left"><Link to="/webgl" >쿰.. 가고싶은대로 간다..</Link></div>
           <div className="header-nav-item inline-block text-left"><Link to="/guest-book" >나.. 왔다간다 !</Link></div>
+          <div className="header-nav-item inline-block text-left"><Link to="/blog" >블로그를,, 해볼까?</Link></div>
         </nav>
       </div>
     </header>
--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ root.render(
         <Route index element={<Home />} />
         <Route path="webgl" element={<WebGL />} />
         <Route path="guest-book" element={<div>나왔다감!!</div>} />
+        <Route path="blog" element={<div>블로그를,, 해볼까?</div>} />
       </Route>
     </Routes>
   </BrowserRouter>
