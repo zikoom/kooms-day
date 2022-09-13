1b3b3d417dabc2058e9f39972b7a5d3bd19e7a02
koom <hunmonhun@gmail.com>
2022-09-13 11:45:41 +0900

    라우팅 + home 추가

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Home from './components/Home'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
