b407607a8be5e20bcd7b04090da868da4d24072f
yoon ji hoon <koom@utplus.co.kr>
2022-09-14 20:29:13 +0900

    왼쪽 부분 위아래 공간 나누기

--- src/jaduguaza/Mine.js
+++ src/jaduguaza/Mine.js
@@ -5,7 +5,10 @@ function Mine(){
   return (
 
     <div className="horror-top-wrapper">
-      <div className="horror-nav"></div>
+      <div className="horror-nav">
+        <div className="horror-nav-upper"></div>
+        <div className="horror-nav-below"></div>
+      </div>
       <div className="horror-header"></div>
       <div className="horror-content"></div>
     </div>
