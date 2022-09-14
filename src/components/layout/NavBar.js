1b2853674d0b0be51b0a4f057e2f5b33ef5964eb
koom <hunmonhun@gmail.com>
2022-09-14 17:07:57 +0900

    class name 다시 바꾸기

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -2,15 +2,13 @@ import { Link } from "react-router-dom";
 
 function NavBar(){
   return (
-    <div className="header-wrapper justify-center">
-        <nav>
-          <div className="header-nav-item inline-block text-left"><Link to="/" >홈</Link></div>
-          <div className="header-nav-item inline-block text-left"><Link to="/webgl" >쿰.. 가고싶은대로 간다..</Link></div>
-          <div className="header-nav-item inline-block text-left"><Link to="/blog" >블로그를,, 해볼까?</Link></div>
-          <div className="header-nav-item inline-block text-left"><Link to="/guest-book" >나.. 왔다간다 !</Link></div>
-          <div className="header-nav-item inline-block text-left"><Link to="/main" >만들어봅시다~</Link></div>
-        </nav>
-      </div>
+    <div className="nav-wrapper flex-container">
+      <div className="nav-flex-child"><Link className="nav-flex-child-link" to="/" >홈</Link></div>
+      <div className="nav-flex-child"><Link className="nav-flex-child-link" to="/webgl" >쿰.. 가고싶은대로 간다..</Link></div>
+      <div className="nav-flex-child"><Link className="nav-flex-child-link" to="/blog" >블로그를,, 해볼까?</Link></div>
+      <div className="nav-flex-child"><Link className="nav-flex-child-link" to="/guest-book" >나.. 왔다간다 !</Link></div>
+      <div className="nav-flex-child"><Link className="nav-flex-child-link" to="/main" >만들어봅시다~</Link></div>
+    </div>
   )
 }
 
