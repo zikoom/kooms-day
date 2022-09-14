3cbebf441f71227defeb612bac2e3394da367a9c
koom <hunmonhun@gmail.com>
2022-09-14 18:05:24 +0900

    자두과자의 원대한꿈..

--- src/jaduguaza/Mine.js
+++ src/jaduguaza/Mine.js
@@ -1,7 +1,13 @@
-function Mine(){
-  return (
-    <div className="horror-top-wrapper">
+import './Mine.scss';
 
+function Mine(){
+  // horror: Mine 페이지 class prefix
+  return (
+
+    <div className="horror-top-wrapper">
+      <div className="horror-nav"></div>
+      <div className="horror-header"></div>
+      <div className="horror-content"></div>
     </div>
   )
 }
