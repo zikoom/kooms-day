a6eae3b705bdadeb5392ee83d1a48ef44a1437d4
yoon ji hoon <koom@utplus.co.kr>
2022-09-14 21:11:56 +0900

    왼쪽 navbar 영역 잡기

--- src/jaduguaza/Mine.js
+++ src/jaduguaza/Mine.js
@@ -6,8 +6,22 @@ function Mine(){
 
     <div className="horror-top-wrapper">
       <div className="horror-nav">
-        <div className="horror-nav-upper"></div>
-        <div className="horror-nav-below"></div>
+        <div className="horror-nav-upper">
+          <hgroup>
+            <h1>
+              BEST
+              <span></span>
+              horror
+              <span></span>
+              SCENE
+            </h1>
+          </hgroup>
+        </div>
+        <div className="horror-nav-below">
+          <div className="below-container">
+
+          </div>
+        </div>
       </div>
       <div className="horror-header"></div>
       <div className="horror-content"></div>
