9985b78d63f0c2dace6f6197536a63e8850495da
koom <hunmonhun@gmail.com>
2022-09-15 12:27:00 +0900

    컨텐트 박스~

--- src/jaduguaza/Clone2.js
+++ src/jaduguaza/Clone2.js
@@ -1,9 +1,24 @@
+import "./Clone2.scss"
+
 function Clone2(){
   return (
-    <div className="Clone2">
-      <header></header>
-      <main></main>
-      <footer></footer>
+    <div className="clone2">
+      <header className="clone2"></header>
+      <main className="clone2">
+        <div className="content-box-container">
+          <div className="content-box">
+            <div className="left-half">
+
+            </div>
+            <div className="right-half">
+
+            </div>
+            {/* <img src="https://cdn.shopify.com/s/files/1/0086/4865/4895/files/Tote_Bag_376x376_crop_center.gif?v=1653663698" alt="img 입니다."></img>
+            <div></div> */}
+          </div>
+        </div>
+      </main>
+      <footer className="clone2"></footer>
     </div>
   )
 }
