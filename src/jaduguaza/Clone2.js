29a9ae55e0a59b88f0e2f9195752d1d40951981c
koom <hunmonhun@gmail.com>
2022-09-20 11:53:37 +0900

    일단 지우기..

--- src/jaduguaza/Clone2.js
+++ src/jaduguaza/Clone2.js
@@ -8,17 +8,7 @@ function Clone2(){
         <div className="content-box-container">
           <div className="content-box">
             <div className="left-half">
-              <picture>
-                {/* <source media="(max-width: 375px)" srcSet="//cdn.shopify.com/s/files/1/0086/4865/4895/files/Tote_Bag_376x376_crop_center.gif?v=1653663698"></source>
-                <source media="(max-width: 699px) and (min-width: 376px)" srcSet="//cdn.shopify.com/s/files/1/0086/4865/4895/files/Tote_Bag_699x699_crop_center.gif?v=1653663698"></source>
-                <source media="(min-width: 700px) and (max-width: 1280px)" srcSet="//cdn.shopify.com/s/files/1/0086/4865/4895/files/Tote_Bag_640x640_crop_center.gif?v=1653663698 1x,
-//cdn.shopify.com/s/files/1/0086/4865/4895/files/Tote_Bag_1280x1280_crop_center.gif?v=1653663698 2x"></source>
-                <source media="(min-width: 1281px) and (max-width: 1600px)" srcSet="//cdn.shopify.com/s/files/1/0086/4865/4895/files/Tote_Bag_800x800_crop_center.gif?v=1653663698 1x,
-//cdn.shopify.com/s/files/1/0086/4865/4895/files/Tote_Bag_1600x1600_crop_center.gif?v=1653663698 2x"></source>
-                <source media="(min-width: 1601px)" srcSet="//cdn.shopify.com/s/files/1/0086/4865/4895/files/Tote_Bag_960x960_crop_center.gif?v=1653663698 1x,
-//cdn.shopify.com/s/files/1/0086/4865/4895/files/Tote_Bag_1920x1920_crop_center.gif?v=1653663698 2x"></source>
-                <img src="//cdn.shopify.com/s/files/1/0086/4865/4895/files/Tote_Bag_960x960_crop_center.gif?v=1653663698" alt="이미지 입니다"></img> */}
-              </picture>
+
               <div>
 
               </div>
