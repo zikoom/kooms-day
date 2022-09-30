e08ec76abf43dc0c0935e241f4c27942bfb29ac3
koom <hunmonhun@gmail.com>
2022-09-30 12:34:05 +0900

    네브바 로그인 영역 공간 잡기

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,8 +1,20 @@
 import { Link } from "react-router-dom";
+import './NavBar.scss'
 
 function NavBar(){
+  let isLogined = true;
   return (
     <nav className="nav">
+      {
+        isLogined ?
+        <div className="user-info">
+          <div className="user-name">OOO 님!</div>
+          <div>화녕 합니다!!</div>
+          <div>압도적 웰컴!!</div>
+        </div> :
+        null
+
+      }
       <div className="nav-flex-child"><Link className="nav-flex-child-link" to="/" >홈</Link></div>
       <div className="nav-flex-child"><Link className="nav-flex-child-link" to="/login" >로그인</Link></div>
       <div className="nav-flex-child"><Link className="nav-flex-child-link" to="/webgl" >쿰.. 가고싶은대로 간다..</Link></div>
