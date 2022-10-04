72021ede9fa9b7b919f722c66af2e9e3f885bc6b
koom <hunmonhun@gmail.com>
2022-10-04 12:32:26 +0900

    굉장히 멋잇는 방명록 페이지 제작중..

--- src/jaduguaza/GuestBook.js
+++ src/jaduguaza/GuestBook.js
@@ -5,15 +5,20 @@ import { firestore } from "../js/firebase"
 export default function GuestBook(){
 
   console.log('firesotre: ', firestore);
+  const boardPublicCheckBoxID = 'public-checkbox'
 
   return (
     <div className="guset-book">
-      <div>
-        <button>테스트 ~</button>
+      <div className="title-wrapper"> 방명록을 남겨보아요~</div>
+      <div className="editor-wrapper">
+        <input type="checkbox" name="" id={boardPublicCheckBoxID} />
+        <label htmlFor={boardPublicCheckBoxID}>비공개</label>
+        <div className="user-info-wrapper">
+          <div className="user-name-wrapper"></div>
+          <div className="user-password-info"></div>
+        </div>
       </div>
-      <div className="title-wrapper"></div>
-      <div className="editor-wrapper"></div>
-      <div className="board-wrapper"></div>
+      <div className="title-password"></div>
       <div className="pagination-wrapper"></div>
     </div>
 
