554d6edc7c0246c9c26fa176c40fe9b045025181
koom <hunmonhun@gmail.com>
2022-10-05 15:31:56 +0900

    미사용 파일 / 코드 제거

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
-import firebaseSetter from "./reducers/firebaseSetter";
 import loginManager from "./reducers/loginManager";
 
 
@@ -8,7 +7,6 @@ import loginManager from "./reducers/loginManager";
 
 export default configureStore({
   reducer: {
-    firestoreSetter: firebaseSetter,
     loginManager: loginManager,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
