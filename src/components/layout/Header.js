c1d0939b09be18755921dd079ce416c942d45d36
koom <hunmonhun@gmail.com>
2023-01-19 12:32:29 +0900

    테스트 햄버거 메뉴 적용

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,8 @@
 function Hedaer(){
+  const testURL = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6q3r64OKSdNldzFVlA_ObmKvoVNBE0YLqdw&usqp=CAU'
   return (
     <header className="header">
+      <img className="hamburgur" src={testURL} alt='haha' />
       <div>header</div>
     </header>
   )
