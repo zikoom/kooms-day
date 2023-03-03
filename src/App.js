c770f88bcd10e0e907110536aaf1e41292ea3d5b
koom <hunmonhun@gmail.com>
2023-03-03 15:08:38 +0900

    oauth json redux 작업

--- src/App.js
+++ src/App.js
@@ -3,25 +3,15 @@ import { useEffect } from 'react';
 import Layout from './components/layout/Layout';
 
 import { useDispatch } from 'react-redux';
-import { SET_OAUTH_MANAGER } from './action/oauth_actions';
-import getOauthSecret from './js/oauth';
+import loadOauthSecret from './js/oauth';
 
 function App() {
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    ( async () => {
-      const result = await getOauthSecret();
-
-      const {auth_uri, client_id, client_secret} = result;
-      dispatch(SET_OAUTH_MANAGER({
-        auth_uri,
-        client_id,
-        client_secret
-      }))
-    })()
-  }, [])
+    loadOauthSecret(dispatch)
+  })
 
 
 
--- src/js/oauth.js
+++ src/js/oauth.js
@@ -1,15 +1,29 @@
 
+import { SET_OAUTH_MANAGER } from "../action/oauth_actions";
 
-// in public folder
 const secretJsonPath = '/oauth/googld_oauth_client_secret.json'
 
-export default async function getOauthSecret () {
+
+/**
+ *
+ * @param {*} dispatch
+ * @description oatuh json 로딩후 redux에 값 세팅
+ */
+export default async function loadOauthSecret (dispatch) {
+  if(!dispatch) throw new Error('no dispatch');
 
   try{
-    const parsedJson = await (await fetch(secretJsonPath, {headers: {Accept: 'application/json'}})).json()
-    return parsedJson.web;
+   const parsedJson = await (await fetch(secretJsonPath, {headers: {Accept: 'application/json'}})).json()
+   const {auth_uri, client_id, client_secret} = parsedJson.web;
+
+   dispatch(SET_OAUTH_MANAGER({
+    auth_uri,
+    client_id,
+    client_secret
+  }))
+
   }catch(e){
-    console.log('call oauth secret json error: ', e);
-    return false;
+   console.log('call oauth secret json error: ', e);
   }
+
 }
