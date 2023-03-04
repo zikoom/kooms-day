d71a2db93156fbdf2f3c66aac867b3ac85910d2a
yoon ji hoon <koom@utplus.co.kr>
2023-03-05 01:35:56 +0900

    햄버거 이미지 사이즈 조절

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,7 +2,18 @@ function Hedaer({toggleMobileMenu}){
   const testURL = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6q3r64OKSdNldzFVlA_ObmKvoVNBE0YLqdw&usqp=CAU'
   return (
     <header className="header">
-      <img className="hamburgur" src={testURL} onClick={toggleMobileMenu} alt='haha' />
+
+      {/* mobile header */}
+      <div className="header-moblie">
+        <img className="hamburgur" src={testURL} onClick={toggleMobileMenu} alt='haha' />
+
+      </div>
+
+      {/* PC header */}
+      <div className="header-pc">
+
+      </div>
+
       <div>header</div>
     </header>
   )
