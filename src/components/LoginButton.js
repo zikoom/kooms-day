d6fbe388a25fec19eb63896b7446b19a1344ba84
koom <hunmonhun@gmail.com>
2023-03-24 15:36:56 +0900

    test

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -8,7 +8,8 @@ export default function LoginButton () {
 
   const requestGoogleLogin = async () => {
     const res = await loginRequestURL();
-   window.location.href = res.data.url;
+  //  window.location.href = res.data.url;
+    console.log('res: ', res);
   }
 
   return (
