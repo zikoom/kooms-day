bb7d254e01860fd25b8e04db6cbca78fe7715be2
koom <hunmonhun@gmail.com>
2023-03-24 18:36:01 +0900

    authCode 수신 확인

--- src/components/auth/Oauth2callback.js
+++ src/components/auth/Oauth2callback.js
@@ -1,7 +1,19 @@
+import { useSearchParams } from "react-router-dom"
 /**
  *  query key:code 에 전달되는 authCode를 사용하여 유저 정보 요청하기
  */
 export default function Oauth2callback(){
+  const [ searchParams, setSearchParams ] = useSearchParams();
+  let queryKeys = [];
+  for(let entry of searchParams.entries())  {queryKeys.push(entry)}
+  const authCode = searchParams.get('code');
+
+  console.log('queryKeys: ', queryKeys);
+  queryKeys.map(key => searchParams.delete(key[0]));
+  setSearchParams(searchParams);
+
+  console.log('authCode: ', authCode);
+
   return (
     <div>
       <h1>콜백 처리 페이지</h1>
