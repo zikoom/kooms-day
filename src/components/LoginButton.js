3ca48715a7a8a8f49f22de37a4980422a2b49faa
koom <hunmonhun@gmail.com>
2023-03-29 12:14:47 +0900

    로그인 이동시 loading 화면 띄우기

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { SET_LOADING_DISPLAY } from '../action/loading';
 
 const loginRequestURL = () => {
   return axios.get(process.env.REACT_APP_SERVER_PATH + '/auth/googleOauthURL')
@@ -6,13 +8,17 @@ const loginRequestURL = () => {
 
 export default function LoginButton () {
 
+  const dispatch = useDispatch();
+
   const requestGoogleLogin = async () => {
+    dispatch(SET_LOADING_DISPLAY(true));
     try {
       const res = await loginRequestURL();
       window.location.href = res.data.url;
-
     } catch (error) {
       console.log('loginRequestURL err. ', error);
+    } finally{
+      dispatch(SET_LOADING_DISPLAY(false));
     }
   }
 
