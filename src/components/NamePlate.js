103e9a0136d8ae8ce11093fa2b2df746f796cd66
koom <hunmonhun@gmail.com>
2023-04-11 16:05:04 +0900

    유저 썸네일 작업

--- src/components/NamePlate.js
+++ src/components/NamePlate.js
@@ -6,17 +6,18 @@
 import PropTypes from 'prop-types'
 
 import styles from 'assets/scss/components/NamePlate.module.scss'
+import { useSelector } from 'react-redux';
 
 
 
 export default function NamePlate({name}){
-  console.log('styles: ', styles);
+  const userThumbnail = useSelector(state => state.firebaseManager.userinfo.photoURL);
+  console.log('userThumbnail: ', userThumbnail);
   return (
-    // <div style={{position: 'relative', width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-    // <div style={{backgroundImage: `url(${'./assets/img/NameFlower4.svg'})`, position: 'relative', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
     <div className={styles['plate-container']}>
+      <div className={styles['plate-content']} style={{backgroundImage: userThumbnail.length > 0 ? `url(${userThumbnail})` : ''}}>
 
-      <p> {name} </p>
+      </div>
     </div>
   )
 }
