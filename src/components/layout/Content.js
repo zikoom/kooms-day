2e33aea5ec9c4e73711f462fb865b578c6c87527
koom <hunmonhun@gmail.com>
2023-04-13 18:20:42 +0900

    컨텐츠 구조 css

--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -4,7 +4,14 @@ function Content(){
   return (
     <main>
       {/* <Outlet /> */}
-      <Chat />
+      {/* <div className='content'> 하나 </div>
+      <div className='content'> 둘 </div>
+      <div className='content'> 셋 </div>
+      <div className='content'> 넷 </div> */}
+      <div className='content'>
+        <Chat />
+      </div>
+      <div className='content'></div>
     </main>
   )
 }
